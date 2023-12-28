13fdf4c794bf6aea43af5fb37cd495f0cf89c479
rannnayy <rani.api3939@gmail.com>
2023-12-28 23:50:30 +0700

    feat: symmetrical 2 keys

---
+++ symmetrical-2-keys/decrypt.js
@@ -0,0 +1,40 @@
+const cyrb53 = (str, seed = 0) => {
+    let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+    for(let i = 0, ch; i < str.length; i++) {
+        ch = str.charCodeAt(i);
+        h1 = Math.imul(h1 ^ ch, 2654435761);
+        h2 = Math.imul(h2 ^ ch, 1597334677);
+    }
+    h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+    h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+    h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+    h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+  
+    return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
+document.getElementById('file').onchange = function(event) {
+    var file = event.target.files[0];
+    var fileReader = new FileReader();
+    fileReader.onload = function() {
+        var typedarray = new Uint8Array(this.result);
+        // console.log(typedarray);
+        var hashkey = cyrb53(document.getElementById('password').value);
+        var username = cyrb53(document.getElementById('username').value);
+        var decryptedarray = typedarray.map(function(e) {
+            return (e - hashkey) % username;
+        });
+        // console.log(hashkey);
+        // console.log(decryptedarray);
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        var blob = new Blob([decryptedarray], {type: "octet/stream"});
+        var url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = "decrypted.pdf";
+        a.click();
+        window.URL.revokeObjectURL(url);
+    }
+    fileReader.readAsArrayBuffer(file);
+}
